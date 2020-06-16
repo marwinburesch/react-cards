d389d9f663cf57476b5235caf1127a14ef7b3ca2
marwinburesch <marwin@posteo.de>
2020-06-16 17:28:50 +0200

    Add second input

--- src/components/Footer/NewsletterForm.js
+++ src/components/Footer/NewsletterForm.js
@@ -2,21 +2,37 @@ import React from "react";
 
 export default function NewsletterForm() {
   const [email, setEmail] = React.useState("");
+  const [firstName, setFirstName] = React.useState("");
 
   function handleChange(event) {
-    const valueFromEvent = event.target.value;
-    setEmail(valueFromEvent);
+    const targetValue = event.target.value;
+    const targetName = event.target.name;
+    if (targetName === "email") {
+      setEmail(targetValue);
+    } else if (targetName === "firstName") {
+      setFirstName(targetValue);
+    }
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("Thanks! The newsletter will be sent to: " + email);
+    console.log(
+      `Thanks, ${firstName}! The newsletter will be sent to: ${email}`
+    );
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <input
+        name="email"
         value={email}
+        placeholder="email"
+        onChange={handleChange}
+        type="text"
+      />
+      <input
+        name="firstName"
+        value={firstName}
         placeholder="email"
         onChange={handleChange}
         type="text"
