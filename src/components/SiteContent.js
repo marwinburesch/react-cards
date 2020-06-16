725883df62995b41a04d9d9d65bd7b36a48d5556
marwinburesch <marwin@posteo.de>
2020-06-16 10:47:23 +0200

    Add Card component base structure

--- src/components/SiteContent.js
+++ src/components/SiteContent.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./SiteContent.css";
+import Card from "./Card/Card";
 
 export default function SiteContent() {
-  return <main className="flex-grow">content</main>;
+  return (
+    <main className="flex-grow">
+      <Card />
+    </main>
+  );
 }
