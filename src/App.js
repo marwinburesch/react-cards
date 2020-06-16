f14177a002250aee2b9ef64de3e4ea268c22b57e
marwinburesch <marwin@posteo.de>
2020-06-16 10:31:28 +0200

    Add styling to SiteContent
    Add flexbox to App and flex-grow to main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./App.css";
 import SiteHeader from "./components/SiteHeader";
 import SiteContent from "./components/SiteContent";
 import SiteFooter from "./components/SiteFooter";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <SiteHeader />
       <SiteContent />
       <SiteFooter />
--- src/components/SiteContent.js
+++ src/components/SiteContent.js
@@ -1,5 +1,6 @@
 import React from "react";
+import "./SiteContent.css";
 
 export default function SiteContent() {
-  return <main>content</main>;
+  return <main className="flex-grow">content</main>;
 }
