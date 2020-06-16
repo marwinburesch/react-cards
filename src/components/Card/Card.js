4af2aca7bf22138ca29871aa50c5139b7f4d074c
marwinburesch <marwin@posteo.de>
2020-06-16 11:00:01 +0200

    Add CardText component base

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Card.css";
+import CardText from "./CardText";
 
 export default function Card() {
-  return <article className="card"></article>;
+  return (
+    <article className="card">
+      <CardText />
+    </article>
+  );
 }
---
+++ src/components/Card/CardText.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CardText.css";
+
+export default function CardText() {
+  return (
+    <div className="card-text">
+      <h2>Card title</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium
+        veniam accusantium alias dolorum ipsam voluptatum harum ab tempora natus
+        officia!
+      </p>
+    </div>
+  );
+}
