aa7bf7e55782c8273b45d43aea9c24945ce5ca62
marwinburesch <marwin@posteo.de>
2020-06-17 12:30:07 +0200

    Add fetchAnimals, call onClick

--- src/App.js
+++ src/App.js
@@ -7,29 +7,31 @@ import SiteFooter from "./components/SiteFooter";
 const animalArray = [
   {
     title: "Bisons",
-    text:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium veniam accusantium alias dolorum ipsam voluptatum harum ab tempora natus officia!",
-    imageSource:
-      "https://images.unsplash.com/photo-1490260400179-d656f04de422?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    text: "Loading...",
+    imageSource: "",
   },
   {
     title: "Wolves",
-    text:
-      "Laudantium Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem veniam accusantium alias dolorum ipsam voluptatum harum ab tempora natus officia!",
-    imageSource:
-      "https://images.unsplash.com/photo-1590420485404-f86d22b8abf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    text: "Loading...",
+    imageSource: "",
   },
 ];
+
+async function fetchAnimals() {
+  return fetch("https://api.jsonbin.io/b/5ee9c253ccc9877ac37d56e1")
+    .then((response) => response.json())
+    .then((data) => data.animals);
+}
 
 function App() {
   const [content, setContent] = React.useState(animalArray[0]);
 
   function setBisonsAsContent() {
-    setContent(animalArray[0]);
+    fetchAnimals().then((response) => setContent(response[0]));
   }
 
   function setWolvesAsContent() {
-    setContent(animalArray[1]);
+    fetchAnimals().then((response) => setContent(response[1]));
   }
 
   return (
